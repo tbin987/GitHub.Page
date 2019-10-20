b5e143103d758a781beed2a6230d9a708d094f7a
tbin987 <tbin987@gmail.com>
2019-10-20 23:19:24 +0800

    Update packages Remove hexo-admin module

--- themes/next/scripts/helpers/next-url.js
+++ themes/next/scripts/helpers/next-url.js
@@ -1,15 +1,12 @@
-/**
- * next-url.js | https://theme-next.org/api/helpers/next-url/
- */
-
 /* global hexo */
 
 'use strict';
 
+const { htmlTag } = require('hexo-util');
+const url = require('url');
+
 hexo.extend.helper.register('next_url', function(path, text, options) {
-  var htmlTag = require('hexo-util').htmlTag;
-  var config = this.config;
-  var url = require('url');
+  const { config } = this;
   var data = url.parse(path);
   var siteHost = url.parse(config.url).hostname || config.url;
 
@@ -22,7 +19,7 @@ hexo.extend.helper.register('next_url', function(path, text, options) {
   if (theme.exturl && data.protocol && data.hostname !== siteHost) {
     tag = 'span';
     exturl = 'exturl';
-    var encoded = new Buffer(path).toString('base64');
+    var encoded = Buffer.from(path).toString('base64');
     attrs = {
       class     : exturl,
       'data-url': encoded
@@ -66,5 +63,5 @@ hexo.extend.helper.register('next_url', function(path, text, options) {
     }
   }
 
-  return htmlTag(tag, attrs, text);
+  return htmlTag(tag, attrs, text, false);
 });
